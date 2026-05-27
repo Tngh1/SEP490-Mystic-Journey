0b167e7f92b73e0a2cf293f22525ac4ad75bace3
mirramirrayumeume <hongtaiphan98@gmail.com>
2026-05-27 16:00:41 +0700

    Turn/OffPanelMain+ListItem

---
+++ Assets/Scripts/Screen/Main/inventory/Data/InventoryItemData.cs
@@ -0,0 +1,17 @@
+[System.Serializable]
+public class InventoryItemData
+{
+    public int inventoryId;
+
+    public int itemId;
+
+    public string itemName;
+
+    public int quantity;
+
+    public string rarity;
+
+    public string iconUrl;
+
+    public string slotType;
+}
